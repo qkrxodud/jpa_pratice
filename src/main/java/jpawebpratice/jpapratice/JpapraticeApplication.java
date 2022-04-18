5b33ebdeea2869926f7c1e9431ed6e051fa85504
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-18 13:07:40 +0900

    *init_commit

---
+++ src/main/java/jpawebpratice/jpapratice/JpapraticeApplication.java
@@ -0,0 +1,13 @@
+package jpawebpratice.jpapratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpapraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpapraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpawebpratice/jpapratice/JpapraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpawebpratice.jpapratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpapraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
